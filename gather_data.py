a7405df849010a9e9abcc9c72727146e79bd87a0
dkoslicki <dmkoslicki@gmail.com>
2022-02-26 17:22:16 -0500

    add a bit of timing code so you can see what to expect

--- gather_data.py
+++ gather_data.py
@@ -1,4 +1,14 @@
 from fits_stats import *
+
+# Just process a single file to test for speed
+from time import time
+fits_file_name = "/Users/dmk333/Dropbox/Astrophotography/FocusTest/L_Ha_2509_300s.fit"
+t1 = time()
+res = get_star_stats_fits(fits_file_name, plot_stats=False)
+t2 = time()
+print(f"Time to process a single file:{t2-t1}")
+input("Press Enter to continue...")
+
 dir_name = "/Users/dmk333/Dropbox/Astrophotography/FocusTest/"
 df = get_stats_dir(dir_name)
 df.to_csv(os.path.join(dir_name, "stats.csv"))
